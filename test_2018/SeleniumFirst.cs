12cdc2c4adbee02f9b24aa0c948a2e484f25e6f5
fr0sya <yaroksana@gmail.com>
2018-10-14 11:46:15 +0300

    files

---
+++ test_2018/SeleniumFirst.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace test_2018
+{
+    [TestFixture]
+    class SeleniumFirst
+    {
+        //private string baseURL;
+        //private object verificationErrors;
+        private IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void BeforeAllMethods()
+        {
+            //driver = new ChromeDriver();
+            driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com.ua/");
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllMethods()
+        {
+            driver.Quit();
+        }
+
+
+        [Test]
+        public void TheFluxday1Test()
+        {
+            //IWebDriver driver = new FirefoxDriver();
+            //IWebDriver driver = new ChromeDriver();
+           // driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            //driver.Navigate().GoToUrl("https://www.google.com.ua/");
+            //
+            driver.FindElement(By.Id("lst-ib")).Click();
+            driver.FindElement(By.Id("lst-ib")).Clear();
+            driver.FindElement(By.Id("lst-ib")).SendKeys("selenium ide" + Keys.Enter);
+            Thread.Sleep(2000);
+            //
+            driver.FindElement(By.LinkText("Selenium IDE")).Click();
+           // Thread.Sleep(2000);
+            driver.FindElement(By.LinkText("Download")).Click();
+           //
+            Assert.AreEqual("Selenium IDE is a Chrome and Firefox plugin which records and plays back user interactions with the browser.", 
+                driver.FindElement(By.CssSelector("a[name=\"selenium_ide\"] > p")).Text);
+            //
+           // driver.Quit();
+            
+        }
+    }
+}
---
+++ test_2018/UnitTest1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace test_2018
+{
+    // [TestClass]
+    [TestFixture]
+    public class UnitTest1
+    {
+        //[TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("start");
+            //int i = 0;
+            double i = 0;
+            Console.WriteLine("(i + 1) / i = " + (i + 1) / i);
+            Console.WriteLine("done");
+            //throw new Exception("ha ha ha ");
+        }
+
+        [OneTimeSetUp]
+        public void BeforeAllMethods()
+        {
+            Console.WriteLine("[OneTimeSetUp] BeforeAllMethods()");
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllMethods()
+        {
+            Console.WriteLine("[OneTimeTearDown] AfterAllMethods()");
+        }
+
+
+        [SetUp]
+        public void SetUp()
+        {
+                Console.WriteLine("[SetUp] SetUp()");
+        }
+
+        [TearDown]
+        public void TearnDoun()
+        {
+            Console.WriteLine("[TearnDoun] TearnDoun()");
+        }
+
+        [Test]
+        public void TestMethod1(
+            [Values("Hello", "World")]
+            string s,
+            [Random(1,10,5)]
+            int x
+            )
+        {
+            Console.WriteLine("Ok, s = " + s + " x = " + x);
+        }
+
+        [TestCase(5, ExpectedResult = true)]
+        //[TestCase(-15, ExpectedResult = false)]
+        public bool TestMethod2(int x)
+        {
+            return x > 0;
+        }
+
+
+        //DataProvider
+        private static readonly object[] ConverterData =
+        {
+            new object[] { 65, 'A' },
+            new object[] { 97, 'a'},
+            new object[] { 98, 'b'}
+        };
+
+       // [Test, TestCaseSource(nameof(ConverterData))]
+        public void TestMethod3(int x, char c)
+        {
+            char expexted = c;
+            char actual = (char)x;
+            Assert.AreEqual(expexted, actual);
+        }
+    }
+}
